224278d1eb355be29e5f19ec91840ca2ca37ab84
Cybersix03 <contact@spardo.ch>
2016-09-16 16:44:13 +0200

    Creates the crud

---
+++ core/php/crud/config/Database.php
@@ -0,0 +1,85 @@
+<?php // config/Database.php
+
+class Database
+{
+    private $pdo = null;
+
+    public function __construct()
+    {
+        $this->db_name = "test";
+        $this->db_user = "admin";
+        $this->db_pass = "toor";
+        $this->db_host = "localhost";
+        $this->db_port = 8889 ;
+    }
+
+    // BDD connexion
+    private function getPDO()
+    {
+        if ($this->pdo === null) {
+
+            try {
+                // DSN
+                $pdo = new PDO("mysql:dbname=" . $this->db_name . ";host=" . $this->db_host . ";port=". $this->db_port, $this->db_user, $this->db_pass);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $pdo->exec("SET CHARACTER SET utf8");
+
+                $this->pdo = $pdo;
+
+            } catch (PDOException $e) {
+                echo 'Pas de connexion avec la BDD : ' . $e->getMessage();
+                die();
+            }
+
+        }
+
+        return $this->pdo;
+    }
+    // simple requeste
+    public function query($statement)
+    {
+        $req  = $this->getPDO()->query($statement);
+        $data = $req->fetchAll(PDO::FETCH_OBJ);
+
+        return $data;
+    }
+
+    // prepare requeste
+    public function prepare($statement, $attributes = array())
+    {
+        $query  = explode(" ", $statement);
+        // Récupération du 1èr mot
+        $option = strtolower(array_shift($query));
+
+        $req = $this->getPDO()->prepare($statement);
+        $req->execute($attributes);
+
+        if ($option == "select" || $option == "show") {
+
+            if ($req->rowCount() > 0) {
+                $data = $req->fetchAll(PDO::FETCH_CLASS);
+                return $data;
+            }
+
+        } elseif ($option == "insert" || $option == "update" || $option == "delete") {
+
+            if ($option == "insert") {
+                // Valeur id inséré
+                return $this->getPDO()->lastInsertId();
+            } else {
+                return $req->rowCount();
+            }
+
+        }
+    }
+
+    // one row
+    public function row($statement, $attributes = array())
+    {
+        $req = $this->getPDO()->prepare($statement);
+        $req->execute($attributes);
+        $data = $req->fetch(PDO::FETCH_ASSOC);
+
+        return $data;
+    }
+}
